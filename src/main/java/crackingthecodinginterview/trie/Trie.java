b09a252daeae714f6e348f487cb2e855a00c215b
abhishekg2 <abhishekg2@vmware.com>
2024-03-25 12:11:24 -0700

    Cracking coding interview solutions

--- src/main/java/crackingthecodinginterview/trie/Trie.java
+++ src/main/java/crackingthecodinginterview/trie/Trie.java
@@ -46,6 +46,49 @@ public class Trie {
         return curr.isWordEnd;
     }
 
+    public boolean startsWith(String prefix) {
+        if(prefix == null || prefix.length() == 0)
+            throw new IllegalArgumentException("invalid input");
+        TrieNode curr = root;
+        for(char c : prefix.toCharArray()){
+            if(!curr.neighbours.containsKey(c)){
+                return false;
+            } else{
+                curr = curr.neighbours.get(c);
+            }
+        }
+        return !curr.neighbours.isEmpty();
+    }
+
+    public HashSet<String> findAllWordsStartingWithGivenPrefix(final String prefix) {
+        if(prefix == null || prefix.length() == 0)
+            throw new IllegalArgumentException("invalid input");
+        TrieNode curr = root;
+        HashSet<String> hset = new HashSet<>();
+        for(char c : prefix.toCharArray()){
+            if(!curr.neighbours.containsKey(c)){
+                return hset;
+            } else{
+                curr = curr.neighbours.get(c);
+            }
+        }
+        accumulateAllWords(hset, curr, prefix,"");
+        return hset;
+    }
+
+    private  void accumulateAllWords(
+            final HashSet<String> hset,
+            final TrieNode root,
+            final String prefix,
+            final String suffix) {
+
+        if(root.isWordEnd)
+            hset.add(prefix + suffix);
+        for(TrieNode neighbor: root.neighbours.values()){
+            accumulateAllWords(hset, neighbor, prefix, suffix + neighbor.c);
+        }
+    }
+
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.addWord("abhishek");
@@ -54,5 +97,7 @@ public class Trie {
         System.out.println(trie.search("abhishek"));
         System.out.println(trie.search("abhisk"));
         System.out.println(trie.search("baklol"));
+        System.out.println(trie.startsWith("baklol"));
+        System.out.println(trie.findAllWordsStartingWithGivenPrefix("abhi"));
     }
 }
