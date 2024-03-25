90266d55e473dcb3b9e9170427d70f87b83a3eea
abhishekg2 <abhishekg2@vmware.com>
2024-03-25 09:59:44 -0700

    Cracking coding interview solutions

---
+++ src/main/java/crackingthecodinginterview/trie/Trie.java
@@ -0,0 +1,58 @@
+package crackingthecodinginterview.trie;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class TrieNode{
+    char c;
+    boolean isWordEnd;
+    HashMap<Character, TrieNode> neighbours = new HashMap<>();// neighbours
+
+    public TrieNode(){}
+    public TrieNode(char c){
+        this.c = c;
+    }
+}
+public class Trie {
+    private TrieNode root = new TrieNode();
+
+    //O(K) where K= length of word
+    public void addWord(final String word){
+        if(word == null || word.length() == 0)
+            throw new IllegalArgumentException("invalid input");
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            if(!curr.neighbours.containsKey(c)){
+                TrieNode node = new TrieNode(c);
+                curr.neighbours.put(c, node);
+            }
+            curr = curr.neighbours.get(c);
+        }
+        curr.isWordEnd = true;
+    }
+
+    //O(K) where K= length of word
+    public boolean search(final String word){
+        if(word == null || word.length() == 0)
+            throw new IllegalArgumentException("invalid input");
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            if(!curr.neighbours.containsKey(c)){
+                return false;
+            } else{
+                curr = curr.neighbours.get(c);
+            }
+        }
+        return curr.isWordEnd;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.addWord("abhishek");
+        trie.addWord("abhikiki");
+        trie.addWord("baklol");
+        System.out.println(trie.search("abhishek"));
+        System.out.println(trie.search("abhisk"));
+        System.out.println(trie.search("baklol"));
+    }
+}
